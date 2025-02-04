a304531d2c11289ecfd0b1b16dc0ad6049f98c00
Rodrigo Slongo <rodrigo.slongotur@hotmail.com>
2025-02-04 13:58:45 -0300

    front utilizado

---
+++ 3356-java-web-front-main/scripts/getDados.js
@@ -0,0 +1,12 @@
+// URL base da API
+const baseURL = 'http://localhost:8080';
+
+export default function getDados(endpoint) {
+    return fetch(`${baseURL}${endpoint}`)
+        .then(response => {
+            return response.json()
+        })
+        .catch(error => {
+            console.error('Erro ao acessar o endpoint /series/top5:', error);
+        });
+}
